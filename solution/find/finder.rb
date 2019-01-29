a1f821d5d5e3e125f73e8991952147e38ff8b239
Valentyna <valentina.kalinichenko@gmail.com>
2019-01-29 08:45:11 -0500

    Check if target is an integer

--- solution/find/finder.rb
+++ solution/find/finder.rb
@@ -1,6 +1,11 @@
 class Finder
 
   def find(list, target)
+
+    if (!target.kind_of? Integer)
+      raise 'Target is not integer'
+    end
+
     list.each do |array|
       if array.include?(target)
         return array
